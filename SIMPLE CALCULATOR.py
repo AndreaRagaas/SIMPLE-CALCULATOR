326a3591545546ac0eb610cd25768fc26e3ead7b
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-05-08 10:46:48 +0800

    creating options

--- SIMPLE CALCULATOR.py
+++ SIMPLE CALCULATOR.py
@@ -14,3 +14,10 @@ def add(x, y):
 #the function to divide two numbers
 def add(x, y):
     return x / y
+
+#creating options
+print("CHOOSE AN OPERATION")
+print("1.ADD")
+print("2.SUBTRACT")
+print("3.MULTIPLY")
+print("4.DIVIDE")
