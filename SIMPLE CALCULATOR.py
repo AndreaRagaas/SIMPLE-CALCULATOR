e6fb9bf714cac8e840f78c0a8e86c150278cff2c
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-05-08 10:56:37 +0800

    check if choice is one of the four options

--- SIMPLE CALCULATOR.py
+++ SIMPLE CALCULATOR.py
@@ -25,4 +25,13 @@ print("4.DIVIDE")
 #creating an area to put the option
 while True:
     # take input from the user
-    choice = input("Enter the choosen operation: ")
+    choice = input("Enter the choosen operation : ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
