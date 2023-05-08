d3bbf01d27e86f1d42dd85d2361c46e0169290fd
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-05-08 10:50:32 +0800

    creating an area to put the option

--- SIMPLE CALCULATOR.py
+++ SIMPLE CALCULATOR.py
@@ -21,3 +21,8 @@ print("1.ADD")
 print("2.SUBTRACT")
 print("3.MULTIPLY")
 print("4.DIVIDE")
+
+#creating an area to put the option
+while True:
+    # take input from the user
+    choice = input("Enter the choosen operation: ")
