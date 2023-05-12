d561eb6a619dbc6ae41ab8da76de9d6f99c50de5
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-05-12 10:40:48 +0800

    creating the widgets

--- SIMPLE CALCULATOR.py
+++ SIMPLE CALCULATOR.py
@@ -65,46 +65,15 @@ def calculate():
 root = tk.Tk()
 root.title("SIMPLE CALCULATOR")
 
-#creating options
-print("CHOOSE AN OPERATION")
-print("1.ADD")
-print("2.SUBTRACT")
-print("3.MULTIPLY")
-print("4.DIVIDE")
+# creating the widgets
+num1_label = tk.Label(root, text="Enter the first number: ")
+num1_entry = tk.Entry(root)
+num2_label = tk.Label(root, text="Enter the second number: ")
+num2_entry = tk.Entry(root)
 
-#creating an area to put the option
-while True:
-    # take input from the user
-    choice = input("Enter the choosen operation : ")
+operation_var = tk.StringVar(value="Add")
+operation_dropdown = tk.OptionMenu(root, operation_var, "Add", "Subtract", "Multiply", "Divide")
 
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter the first number: "))
-            num2 = float(input("Enter the second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
+calculate_button = tk.Button(root, text="Calculate", command=lambda: [calculate(), ask_again()])
+result_label = tk.Label(root, text="Result:")
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-            
-        # check if user wants to try again
-        # exit if the answer is no
-        try_again = input("Let's do next calculation? (yes/no): ")
-        if try_again == "no":
-            #display thank you message
-            print("Thank You!")
-            break
-          
-    else:
-        print("Invalid Input")
